a7efbd5215ca7d9d5589cc8c21dd196c7f573f3d
vlmrtn <nmartin_02@mail.ru>
2025-04-26 16:32:00 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -3,32 +3,33 @@
 #define INCLUDE_TPQUEUE_H_
 #include <list>
 
+
 template<typename T>
 class TPQueue {
-private:
-   std::list<T> items;
-public:
-   void push(const T& element) {
-      auto it = items.begin();
-      while (it != items.end() && it->prior >= element.prior) {
-         ++it;
-      }
-      items.insert(it, element);
-   }
-   T pop() {
-      T frontElement = items.front();
-      items.pop_front();
-      return frontElement;
-   }
-   bool isEmpty() const {
-      return items.empty();
-   }
-   const T& front() const {
-      return items.front();
-   }
-   size_t size() const {
-      return items.size();
-   }
+ private:
+    std::list<T> lists;
+ public:
+    void push(const T& element) {
+       auto it = items.front();
+       while (it != items.end() && it->prior >= element.prior) {
+          ++it;
+       }
+       items.insert(it, element);
+    }
+    T pop() {
+       T frontElement = items.front();
+       items.pop_front();
+       return frontElement;
+    }
+    bool isEmpty() const {
+       return items.empty();
+    }
+    const T& front() const {
+       return items.front();
+    }
+    size_t size() const {
+       return items.size();
+    }
 };
 
 struct SYM {
